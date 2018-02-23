bec51a87b91f1e6fbe556ab4b3edac9789708310
Michael melvin <mmelvin0581@gmail.com>
2018-02-22 20:13:40 -0500

    continued redux-101

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,28 +3,42 @@ import { createStore } from "redux";
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
+      const incrementBy =
+        typeof action.incrementBy === "number" ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       };
     case "DECREMENT":
+      const decrementBy =
+        typeof action.decrementBy === "number" ? action.decrementBy : 1;
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
       };
     case "RESET":
       return {
         count: 0
       };
+    case "SET":
+      return {
+        count: action.count
+      };
     default:
       return state;
   }
 });
-
-console.log(store.getState());
+// 'watches' the state for changes
+// its return value will make an unsubscription
+// eg -> const unsubscribe = store.subscribe(() => {...});
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
 // Action - an object that gets sent to the store
 // CAPS FOR ACTION TYPES AND UNDERSCORES FOR MULTIPLE WORDS
+// 'type' MUST be provided
 store.dispatch({
-  type: "INCREMENT"
+  type: "INCREMENT",
+  incrementBy: 5
 });
 
 store.dispatch({
@@ -39,4 +53,12 @@ store.dispatch({
   type: "DECREMENT"
 });
 
-console.log(store.getState());
+store.dispatch({
+  type: "DECREMENT",
+  decrementBy: 10
+});
+
+store.dispatch({
+  type: "SET",
+  count: 101
+});
