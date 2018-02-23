4afff57617d0de7cfab51e75d822e7932d158830
Michael melvin <mmelvin0581@gmail.com>
2018-02-22 23:08:28 -0500

    npm uuid installed

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,6 +1,22 @@
 import { createStore, combineReducers } from "redux";
+import uuid from "uuid";
 
 // ADD_EXPENSE
+const addExpense = ({
+  description = "",
+  note = "",
+  amount = 0,
+  createdAt = 0
+} = {}) => ({
+  type: "ADD_EXPENSE",
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
 // REMOVE_EXPENSE
 // EDIT_EXPENSE
 // SET_TEXT_FILTER
@@ -12,6 +28,8 @@ import { createStore, combineReducers } from "redux";
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
+    case "ADD_EXPENSE":
+      return [...state, action.expense];
     default:
       return state;
   }
@@ -40,7 +58,11 @@ const store = createStore(
   })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(addExpense({ description: "Rent", amount: 100 }));
 
 const demoState = {
   expenses: [
