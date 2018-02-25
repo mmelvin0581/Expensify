fc86f1c9e38c87bf42da56cb6d862deb3a530616
Michael melvin <mmelvin0581@gmail.com>
2018-02-24 19:33:15 -0500

    setup responsive filtering for expenses

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
+import ExpenseListFilters from './ExpenseListFilters';
 
 /** Dashboard page */
 const ExpenseDashboardPage = () => (
   <div>
+    <ExpenseListFilters />
     <ExpenseList />
   </div>
 );
--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,18 +1,23 @@
 /** Gets the visible expenses.
  * Takes in the two states of the app expenses and filters which is destructed.
+ *
+ * Returns the filtered and sorted array.
  */
 
 export default (expenses, {
  text, sortBy, startDate, endDate
-}) => expenses
-    .filter((expense) => { // if there is a match, return the match, else not
+}) =>
+  expenses
+    .filter((expense) => {
+      // if there is a match, return the match, else not
       const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
       const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
       const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
       return startDateMatch && endDateMatch && textMatch;
     })
-    .sort((a, b) => { // sort based on the match from above
+    .sort((a, b) => {
+      // sort based on the match from above
       if (sortBy === 'date') {
         return a.createdAt < b.createdAt ? 1 : -1;
       } else if (sortBy === 'amount') {
