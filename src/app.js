a00492fe533942386f91e1a2fd2ba5a63387ef0d
Michael melvin <mmelvin0581@gmail.com>
2018-02-26 13:33:42 -0500

    set up mock library for moment and trivial ExpenseForm tests

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import AppRouter from './routers/AppRouter';
-import configureStore from './store/configureStore';
-import {addExpense} from './actions/expenses';
+import React              from 'react';
+import ReactDOM           from 'react-dom';
+import { Provider }       from 'react-redux';
+import AppRouter          from './routers/AppRouter';
+import configureStore     from './store/configureStore';
+import { addExpense }     from './actions/expenses';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
