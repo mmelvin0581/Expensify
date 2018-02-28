6c8a38624c36f3023c7d659bc9f47014cdff1add
Michael melvin <mmelvin0581@gmail.com>
2018-02-27 20:48:25 -0500

    writing data to firebase tutorial

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,6 +11,23 @@ const config = {
 
 firebase.initializeApp(config);
 
-firebase.database().ref().set({
-  name: 'Michael Melvin'
+const database = firebase.database();
+
+database.ref().set({
+  name: 'Michael Melvin',
+  age: 31,
+  isSingle: false,
+  location: {
+    city: 'Statesboro',
+    country: 'United States'
+  }
+});
+
+// database.ref().set('This is my data.');
+
+database.ref('age').set(32);
+database.ref('location/city').set('Tampa');
+database.ref('attributes').set({
+  height: 74,
+  weight: 165
 });
