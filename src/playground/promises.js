2e755fd0ac5ce6184c4a3afcdd85e70607315924
Michael melvin <mmelvin0581@gmail.com>
2018-02-28 22:04:55 -0500

    test

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,9 +1,9 @@
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    // resolve({
-    //   name: 'Michael',
-    //   age: 26
-    // });
+    resolve({
+      name: 'Michael',
+      age: 26
+    });
     reject('Something went wrong!')
   }, 5000);
 });
@@ -12,6 +12,13 @@ console.log('Before');
 
 promise.then((data) => {
   console.log('1', data);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('This is my other promise');
+    }, 5000);
+  });
+}).then((str) => {
+  console.log('does this run?', str);
 }).catch((error) => {
   console.log('error: ', error);
 });
