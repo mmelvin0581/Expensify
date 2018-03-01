d0cfa502c77958dfca1aa387471f83707311609c
Michael melvin <mmelvin0581@gmail.com>
2018-02-28 23:27:04 -0500

    separate database set up for tests and dev

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyAjtSnURyAvAZuvYBJBOs_4x4dBBE9xotk",
-  authDomain: "expensify-0581.firebaseapp.com",
-  databaseURL: "https://expensify-0581.firebaseio.com",
-  projectId: "expensify-0581",
-  storageBucket: "expensify-0581.appspot.com",
-  messagingSenderId: "905639604755"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
