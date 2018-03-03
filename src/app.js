a3bd5709457b649d6d75a7631e15da0d3b2ff520
Michael melvin <mmelvin0581@gmail.com>
2018-03-02 19:05:28 -0500

    setup logging in with google

--- src/app.js
+++ src/app.js
@@ -1,21 +1,22 @@
-import React              from 'react';
-import ReactDOM           from 'react-dom';
-import { Provider }       from 'react-redux';
-import AppRouter          from './routers/AppRouter';
-import configureStore     from './store/configureStore';
-import { startSetExpenses }     from './actions/expenses';
-import getVisibleExpenses from './selectors/expenses';
+import React                from 'react';
+import ReactDOM             from 'react-dom';
+import { Provider }         from 'react-redux';
+import AppRouter            from './routers/AppRouter';
+import configureStore       from './store/configureStore';
+import { startSetExpenses } from './actions/expenses';
+import getVisibleExpenses   from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import './firebase/firebase';
+import { firebase }         from './firebase/firebase'
 
 const store = configureStore();
 
 const jsx = (
     // Provider gives the app access to the store
     <Provider store={store}>
-        <AppRouter/>
+      <AppRouter/>
     </Provider>
 );
 
@@ -23,4 +24,12 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 store.dispatch(startSetExpenses()).then(() => {
   ReactDOM.render(jsx, document.getElementById('app'));
+});
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log('log in');
+  } else {
+    console.log('log out');
+  }
 });
