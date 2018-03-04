2d4b330fc249bdeb4dce841826b6d651e380a74f
Michael melvin <mmelvin0581@gmail.com>
2018-03-03 19:09:07 -0500

    finished styling and set up a test for the loading page

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LodingPage = () => (
+    <div className="loader"><img className="loader__image" src="/images/loader.gif"/></div>
+);
+
+export default LodingPage;
